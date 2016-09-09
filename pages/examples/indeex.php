ea12b59faee9812a440dcf77cd5b16f77920a76e
pvaghasiya32 <titans1301@gmail.com>
2016-09-09 15:35:07 +0530

    CRUD

---
+++ pages/examples/indeex.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="../../plugins/iCheck/square/blue.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+ 
+<body>
+    <div class="container">
+            <div class="row">
+                <h3>USER TABLE DATA</h3>
+            </div>
+            <div class="row">
+			 <p>
+                    <a href="create.php" class="btn btn-success">Create</a>
+                </p>
+                <table class="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th>id</th>
+					  <th>first_name</th>
+                      <th>last_name</th>
+                      <th>email</th>
+					  <th>password</th>
+					  <th>role</th>
+					  <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  <?php
+                   include 'connect.php';
+                   $pdo = Database::connect();
+                   $sql = 'SELECT * FROM users';
+                   foreach ($pdo->query($sql) as $row) {
+                            echo '<tr>';
+                            echo '<td>'. $row['id'] . '</td>';
+							echo '<td>'. $row['first_name'] . '</td>';
+							echo '<td>'. $row['last_name'] . '</td>';
+                            echo '<td>'. $row['email'] . '</td>';
+                            echo '<td>'. $row['password'] . '</td>';
+							echo '<td>'. $row['role'] . '</td>';
+							echo '<td width=250>';
+                                echo '<a class="btn" href="read.php?id='.$row['id'].'">Read</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-success" href="update.php?id='.$row['id'].'">Update</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Delete</a>';
+                                echo '</td>';
+                            echo '</tr>';
+                   }
+                   Database::disconnect();
+                  ?>
+                  </tbody>
+            </table>
+        </div>
+    </div> <!-- /container -->
+  </body>
+</html>
---
+++ pages/examples/update.php
@@ -0,0 +1,186 @@
+<?php
+    require 'connect.php';
+ 
+    $id = null;
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+     
+    if ( null==$id ) {
+        header("Location: indeex.php");
+    }
+     
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $idError = null;
+        $first_nameError = null;
+        $last_nameError = null;
+		$emailError = null;
+        $passwordError = null;
+        $roleError = null;
+         
+        // keep track post values
+     	$id = $_POST['id'];
+        $first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+		$role = $_POST['role'];
+         
+        // validate input
+        $valid = true;
+        if (empty($id)) {
+            $idError = 'Please enter id';
+            $valid = false;
+        }
+		if (empty($first_name)) {
+            $first_nameError = 'Please enter name';
+            $valid = false;
+        }
+         if (empty($last_name)) {
+            $last_nameError = 'Please enter last_name';
+            $valid = false;
+        }
+        if (empty($email)) {
+            $emailError = 'Please enter Email Address';
+            $valid = false;
+        } else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+            $emailError = 'Please enter a valid Email Address';
+            $valid = false;
+        }
+         if (empty($password)) {
+            $passwordError = 'Please enter password';
+            $valid = false;
+        }
+        if (empty($role)) {
+            $roleError = 'Please enter role';
+            $valid = false;
+        }
+         
+        // update data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "UPDATE users  set id = ?, first_name = ?, last_name = ?, email = ?, password =?, role = ?, WHERE id = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($id,$first_name,$last_name,$email,$password,$role,$id));
+            Database::disconnect();
+            header("Location: indeex.php");
+        }
+    } else {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM users where id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+		$id = $data['id'];
+		$first_name = $data['first_name'];
+        $last_name = $data['last_name'];
+        $email = $data['email'];
+        $password = $data['password'];
+		$role = $data['role'];
+        Database::disconnect();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="../../plugins/iCheck/square/blue.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+ 
+<body>
+    <div class="container">
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Update a Customer</h3>
+                    </div>
+             
+                    <form class="form-horizontal" action="update.php?id=<?php echo $id?>" method="post">
+                      <div class="control-group <?php echo !empty($idError)?'error':'';?>">
+                        <label class="control-label">id</label>
+                        <div class="controls">
+                            <input name="id" type="int"  placeholder="id" value="<?php echo !empty($id)?$id:'';?>">
+                            <?php if (!empty($idError)): ?>
+                                <span class="help-inline"><?php echo $idError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+					  <div class="control-group <?php echo !empty($first_nameError)?'error':'';?>">
+                        <label class="control-label">first_name</label>
+                        <div class="controls">
+                            <input name="first_name" type="text"  placeholder="first_name" value="<?php echo !empty($first_name)?$first_name:'';?>">
+                            <?php if (!empty($first_nameError)): ?>
+                                <span class="help-inline"><?php echo $first_nameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+					  <div class="control-group <?php echo !empty($last_nameError)?'error':'';?>">
+                        <label class="control-label">last_name</label>
+                        <div class="controls">
+                            <input name="last_name" type="text"  placeholder="last_name" value="<?php echo !empty($last_name)?$last_name:'';?>">
+                            <?php if (!empty($last_nameError)): ?>
+                                <span class="help-inline"><?php echo $last_nameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($emailError)?'error':'';?>">
+                        <label class="control-label">email</label>
+                        <div class="controls">
+                            <input name="email" type="text" placeholder="Email Address" value="<?php echo !empty($email)?$email:'';?>">
+                            <?php if (!empty($emailError)): ?>
+                                <span class="help-inline"><?php echo $emailError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+					  <div class="control-group <?php echo !empty($passwordError)?'error':'';?>">
+                        <label class="control-label">password</label>
+                        <div class="controls">
+                            <input name="password" type="password"  placeholder="password" value="<?php echo !empty($password)?$password:'';?>">
+                            <?php if (!empty($passwordError)): ?>
+                                <span class="help-inline"><?php echo $passwordError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($roleError)?'error':'';?>">
+                        <label class="control-label">role</label>
+                        <div class="controls">
+                            <input name="role" type="text"  placeholder="role" value="<?php echo !empty($role)?$role:'';?>">
+                            <?php if (!empty($roleError)): ?>
+                                <span class="help-inline"><?php echo $roleError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Update</button>
+                          <a class="btn" href="indeex.php">Back</a>
+                        </div>
+                    </form>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+</html>
