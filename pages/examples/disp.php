6bd7082af8b8a9bfa025e69214a5f246d06bd9ff
pvaghasiya32 <titans1301@gmail.com>
2016-09-09 09:33:46 +0530

    user display table disp.ph

---
+++ pages/examples/disp.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+//css
+<style>
+table
+{
+border-style:solid;
+border-width:2px;
+border-color:pink;
+}
+</style>
+</head>
+<body bgcolor="#EEFDEF">
+<?php
+$con = mysql_connect("localhost","root","");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+ 
+mysql_select_db("sales", $con);
+ 
+$result = mysql_query("SELECT * FROM users");
+ 
+echo "<table border='1'>
+<tr>
+<th>Id</th>
+<th>first_name</th>
+<th>last_name</th>
+<th>email</th>
+<th>password</th>
+<th>roles</th>
+</tr>";
+ 
+while($row = mysql_fetch_array($result))
+  {
+  echo "<tr>";
+  echo "<td>" . $row['id'] . "</td>";
+  echo "<td>" . $row['first_name'] . "</td>";
+  echo "<td>" . $row['last_name'] . "</td>";
+  echo "<td>" . $row['email'] . "</td>";
+  echo "<td>" . $row['password'] . "</td>";
+  echo "<td>" . $row['role'] . "</td>";
+  echo "</tr>";
+  }
+echo "</table>";
+ 
+mysql_close($con);
+?>
+ </body>
+</html>
